b958cc4ccefa6d39a23c86a35454acd63ef6a718
zyucc <110530125+zeissyusuf@users.noreply.github.com>
2022-09-09 15:51:23 +0530

    Update string_calculator.test.py

--- string_calculator.test.py
+++ string_calculator.test.py
@@ -1,7 +1,7 @@
 import string_calculator
 import unittest
 
-class TypeWiseTest(unittest.TestCase):
+class String_Calculator_Test(unittest.TestCase):
   def GivenEmptyStringZeroIsExpected(self):
     self.assertTrue(string_calculator.Add_Numbers("") == 0)
     
