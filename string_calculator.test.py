83478b8b4e3a02fba1644a3305a37d103a0da2bc
zyucc <110530125+zeissyusuf@users.noreply.github.com>
2022-09-09 15:44:24 +0530

    Update string_calculator.test.py

--- string_calculator.test.py
+++ string_calculator.test.py
@@ -1,1 +1,9 @@
+import string_calculator
+import typewise_alert
 
+class TypeWiseTest(unittest.TestCase):
+  def GivenEmptyStringZeroIsExpected(self):
+    self.assert(typewise_alert.Add("") == 0)
+    
+if __name__=='__main__':
+  unittest.main()
