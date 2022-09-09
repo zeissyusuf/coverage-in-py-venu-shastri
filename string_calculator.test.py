7bf49e3d3204db9a19f4ac212b7349cc9e7d3960
zyucc <110530125+zeissyusuf@users.noreply.github.com>
2022-09-09 16:02:32 +0530

    Delete string_calculator.test.py

--- string_calculator.test.py
+++
@@ -1,9 +0,0 @@
-import string_calculator
-import unittest
-
-class String_Calculator_Test(unittest.TestCase):
-  def GivenEmptyStringZeroIsExpected(self):
-    self.assertTrue(string_calculator.Add("") == 0)
- 
-if __name__=='__main__':
-  unittest.main()
