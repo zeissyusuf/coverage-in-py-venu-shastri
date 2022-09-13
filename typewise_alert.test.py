f22e5755cf624ab53c9408eaa159857df2d789b1
zyucc <110530125+zeissyusuf@users.noreply.github.com>
2022-09-13 12:57:06 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -1,32 +1,15 @@
-#import unittest
-# import unittest
-# 2
-# import typewise_alert
-# 3
-# ​
-# 4
-# ​
-# 5
-# class TypewiseTest(unittest.TestCase):
-# 6
-#   def test_infers_breach_as_per_limits(self):
-# 7
-#     self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
-# 8
-#   def test_infers_breach_as_per_limits(self):
-# 9
-#     self.assertTrue(typewise_alert.infer_breach(160, 50, 100) == 'TOO_HIGH')
-# 10
-#   def test_infers_breach_as_per_limits(self):
-# 11
-#     self.assertTrue(typewise_alert.infer_breach(60, 50, 100) == 'NORMAL')
-# 12
-# 
-# 13
- 
-# 14
-# ​
-# 15
-# if __name__ == '__main__':
-# 16
-#   unittest.main()
+import unittest
+import unittest
+
+import typewise_alert
+
+class TypewiseTest(unittest.TestCase):
+ def test_infers_breach_as_per_limits(self):
+  self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
+  def test_infers_breach_as_per_limits(self):
+   self.assertTrue(typewise_alert.infer_breach(160, 50, 100) == 'TOO_HIGH')
+   def test_infers_breach_as_per_limits(self):
+    self.assertTrue(typewise_alert.infer_breach(60, 50, 100) == 'NORMAL')
+
+if __name__ == '__main__':
+ unittest.main()
